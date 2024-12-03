940d46199800b37f4ed08ed457c60c240f6e1a52
JoseFabioGuimaraes <jfabioguimaraes@gmail.com>
2024-12-03 16:18:30 -0300

    Create pages and SearchField.page.js. Update SearchFieldSteps.js

--- cypress/e2e/steps/SearchFieldSteps.js
+++ cypress/e2e/steps/SearchFieldSteps.js
@@ -1,43 +1,45 @@
 /// <reference types="cypress" />
 
 import { Given, When, Then, And } from "cypress-cucumber-preprocessor/steps";
+import SearchFieldPage from '../../pages/SearchField.page';
 
 Given('I access the main page from IBGE', () => {
-    cy.visit('https://www.ibge.gov.br/');
+    SearchFieldPage.visit();
 });
 
 When('I click on lens icon', () => {
-    cy.get('#btn__busca--mobile').click();
+    SearchFieldPage.clickLensIcon();
 });
 
 And('type {string} in the search field', (search) => {
     if(search === "") {
         //do nothing
-    }
-    else{
-        cy.get('.envolve_logo.mobile-only > .largura-maxima > .nav-container > .pure-u-1 > #busca > .search > .form-inline > #mod-search-searchword').type(search);
+    } else {
+        SearchFieldPage.typeInSearchField(search);
     }
 });
 
 And('click on lens icon again', () => {
-    cy.get('.envolve_logo.mobile-only > .largura-maxima > .nav-container > .pure-u-1 > #busca > .search > .form-inline > .button').click();
+    SearchFieldPage.clickLensIconSearch();
 });
 
 Then('the message {string} should be displayed in the field {string}', (message, campo) => {
-    cy.get(campo).should('contain', message);
+    SearchFieldPage.verifyMessageDisplayed(campo, message);
 });
 
-// Second scenario : Test Close Search Button Mobile
-
+// Steps for the new scenario
 When('I click on the lens icon', () => {
-    cy.get('#btn__busca--mobile').click();
-})
-And('type "Teste" in the search field', ()=> {
-    cy.get('.envolve_logo.mobile-only > .largura-maxima > .nav-container > .pure-u-1 > #busca > .search > .form-inline > #mod-search-searchword').type('População');
-})
+    SearchFieldPage.clickLensIcon();
+});
+
+And('type "Teste" in the search field', () => {
+    SearchFieldPage.typeInSearchField('Teste');
+});
+
 And('click on the close icon', () => {
-    cy.get('#btn__busca--mobile').click();
-})
-Then ('the search field should not be displayed', () => {
-    cy.get('.envolve_logo.mobile-only > .largura-maxima > .nav-container > .pure-u-1 > #busca > .search > .form-inline > #mod-search-searchword').should('not.be.visible');
+    SearchFieldPage.clickCloseIcon();
+});
+
+Then('the search field should not be displayed', () => {
+    SearchFieldPage.verifySearchFieldNotDisplayed();
 });
