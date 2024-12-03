3b135a226a92543ba8fc51d48d8f1e3997f49fca
JoseFabioGuimaraes <jfabioguimaraes@gmail.com>
2024-12-03 11:08:48 -0300

    Update SearchFieldStep.js and SearchField.feature

--- cypress/e2e/features/SearchField.feature
+++ cypress/e2e/features/SearchField.feature
@@ -1,4 +1,4 @@
-Feature: Seach Field 
+Feature: Search Field 
     Test the search field on the IBGE website
 
 Background: Open page
@@ -6,17 +6,17 @@ Background: Open page
 
 Scenario Outline: Test Search Menu Field
     When I click on lens icon 
-    And type <search> in the search field
+    And type "<search>" in the search field
     And click on lens icon again
-    Then the message <message> should be displayed
+    Then the message "<message>" should be displayed in the field "<campo>"
 
     Examples:
-        | search | message | 
-        | "População"  | "Mostrando " |
-        | "" | "Não há resultados para a sua busca." |
+        | search      | message                                | campo                   |
+        | População   | Mostrando                              | .busca__mostrando > p   |
+        |           | Não há resultados para a sua busca.    | b                       |
 
-Scenario: Test Close Search Button
-    When I click on the magnifying glass icon
+Scenario: Test Close Search Button Mobile
+    When I click on the lens icon
     And type "Teste" in the search field
     And click on the close icon
     Then the search field should not be displayed
--- cypress/e2e/steps/SearchFieldSteps.js
+++ cypress/e2e/steps/SearchFieldSteps.js
@@ -1,19 +1,43 @@
 /// <reference types="cypress" />
 
-import { Given,When, Then, And } from "cypress-cucumber-preprocessor/steps/index";
+import { Given, When, Then, And } from "cypress-cucumber-preprocessor/steps";
 
-Given('I access the main page from IBGE', () =>{
+Given('I access the main page from IBGE', () => {
     cy.visit('https://www.ibge.gov.br/');
 });
-When('I click on lens icon', () =>{
+
+When('I click on lens icon', () => {
     cy.get('#btn__busca--mobile').click();
 });
-And('type {string} in the search field', (search) =>{
-    cy.get('.envolve_logo.mobile-only > .largura-maxima > .nav-container > .pure-u-1 > #busca > .search > .form-inline > #mod-search-searchword').type(search)
+
+And('type {string} in the search field', (search) => {
+    if(search === "") {
+        //do nothing
+    }
+    else{
+        cy.get('.envolve_logo.mobile-only > .largura-maxima > .nav-container > .pure-u-1 > #busca > .search > .form-inline > #mod-search-searchword').type(search);
+    }
 });
-And('click on lens icon again', ()=>{
+
+And('click on lens icon again', () => {
     cy.get('.envolve_logo.mobile-only > .largura-maxima > .nav-container > .pure-u-1 > #busca > .search > .form-inline > .button').click();
+});
+
+Then('the message {string} should be displayed in the field {string}', (message, campo) => {
+    cy.get(campo).should('contain', message);
+});
+
+// Second scenario : Test Close Search Button Mobile
+
+When('I click on the lens icon', () => {
+    cy.get('#btn__busca--mobile').click();
 })
-Then('the message {string} should be displayed',(message) =>{
-    cy.get('.busca__mostrando > p').should('contain', message);
+And('type "Teste" in the search field', ()=> {
+    cy.get('.envolve_logo.mobile-only > .largura-maxima > .nav-container > .pure-u-1 > #busca > .search > .form-inline > #mod-search-searchword').type('População');
 })
+And('click on the close icon', () => {
+    cy.get('#btn__busca--mobile').click();
+})
+Then ('the search field should not be displayed', () => {
+    cy.get('.envolve_logo.mobile-only > .largura-maxima > .nav-container > .pure-u-1 > #busca > .search > .form-inline > #mod-search-searchword').should('not.be.visible');
+});
