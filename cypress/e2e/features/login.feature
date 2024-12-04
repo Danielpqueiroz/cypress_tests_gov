e22f838819bd0232122fd065c11eb45aa3b2b9c9
Diego de Oliveira Santana Cipriano <112182138+DiegodOliveira@users.noreply.github.com>
2024-12-03 21:39:36 -0300

    verificação de links em trabalhos e em outros indicadores

--- cypress/e2e/features/login.feature
+++ cypress/e2e/features/login.feature
@@ -8,4 +8,9 @@ Scenario: Navigate to the 'Tábua completa de Mortalidade' page through the side
     When I click on side menu
     And click on "Estatisticas", "Sociais" and "População"
     And click on "Tábuas de Mortalidade"
-    Then the message "Tábuas Completas de Mortalidade" should be displayed
+    And the message "Tábuas Completas de Mortalidade" should be displayed
+    And click to go back to main page "IBGE"
+    Then in main page click in "outros indicadores"
+    And in page click in "IPCA"
+    Then click to go back again to main page "IBGE"
+    And go to side menu and click in "trabalho" e ver dados
--- cypress/e2e/steps/loginSteps.js
+++ cypress/e2e/steps/loginSteps.js
@@ -20,6 +20,30 @@ And('click on "Tábuas de Mortalidade"',()=>{
     cy.get('.box--cinza > .largura-maxima > .conteudo__interna__containercaixa > :nth-child(1) > .tit-pesquisa > .productTitle').click();
 })
 
-Then('the message "Tábuas Completas de Mortalidade" should be displayed', () =>{
-    cy.get('.productTitle').should('have.text','Tábuas Completas de Mortalidade')
+And('the message "Tábuas Completas de Mortalidade" should be displayed', () =>{
+    cy.get('.productTitle').should('have.text','Tábuas Completas de Mortalidade');
+})
+
+And('click to go back to main page "IBGE"', ()=>{
+    cy.get(':nth-child(2) > .pathway').click();
+})
+
+Then('in main page click in "outros indicadores"', ()=>{
+    cy.get('.btn-outros-indicadores').wait(3000).click();
+})
+
+And('in page click in "IPCA"', ()=>{
+    cy.get('#indicador-ipca > th > .nonsprite').click();
+})
+
+Then('click to go back again to main page "IBGE"', ()=>{
+    cy.get(':nth-child(2) > .pathway').click();
+})
+
+And('go to side menu and click in "trabalho" e ver dados', ()=>{
+    cy.get('.envolve_logo.mobile-only > .largura-maxima > .nav-container > .pure-u-1 > #nav-toggle').click();
+    cy.get('[idmenu="1"] > :nth-child(1)').click();
+    cy.get('[style="display: block;"] > .parent.nvcls_ > .nav-header').click();
+    cy.get('[idmenu="4"] > .header').wait(3000).click();
+    cy.get('.box--cinza > .largura-maxima > .conteudo__interna__containercaixa > :nth-child(2) > .tit-pesquisa > .productTitle').click();
 })
